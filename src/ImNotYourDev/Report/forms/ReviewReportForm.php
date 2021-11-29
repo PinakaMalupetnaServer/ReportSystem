134c4c1edc8d38a3437efa7a71569c54356b036f
Frizth Lyco Tatierra <lycol50@gmail.com>
2021-11-30 01:27:04 +0800

    Added Ban Support
    Compatible with or without cucumber plugin

--- src/ImNotYourDev/Report/forms/ReviewReportForm.php
+++ src/ImNotYourDev/Report/forms/ReviewReportForm.php
@@ -6,6 +6,7 @@ use ImNotYourDev\Report\libs\dktapps\pmforms\MenuForm;
 use ImNotYourDev\Report\libs\dktapps\pmforms\MenuOption;
 use ImNotYourDev\Report\Report;
 use pocketmine\Player;
+use pocketmine\Server;
 
 class ReviewReportForm extends MenuForm
 {
@@ -18,16 +19,17 @@ class ReviewReportForm extends MenuForm
         $text = "§7Reportname: §e" . $report["name"] . "\n§7Reporter: §e" . $report["reporter"] . "\n§7Reported player: §e" . $report["player"] . "\n§7Description: §e" . $report["desc"] . "\n§7Notes: §e" . $report["notes"] . "\n\n§7Choose now what u want to do";
         $options = [
             new MenuOption("§1Teleport(only if online)"),
-            new MenuOption("§cBan player(soon)"),
+            new MenuOption("§cBan player"),
             new MenuOption("§4Delete report"),
             new MenuOption("§aBack")
         ];
 
         Report::getInstance()->setReviewed($report["nestdir"]);
 
-        parent::__construct($title, $text, $options, function (Player $player, $data) : void {
+        parent::__construct($title, $text, $options, function (Player $player, $data)  : void {
+            $target = Report::getInstance()->getServer()->getPlayer($this->report["player"]);
             if($data == 0){
-                $target = Report::getInstance()->getServer()->getPlayer($this->report["player"]);
+                //$target = Report::getInstance()->getServer()->getPlayer($this->report["player"]);
                 if($target == true){
                     $player->teleport($target->asPosition());
                     $player->sendMessage(Report::getInstance()->prefix . "You were teleported to §e" . $target->getName() . "§7!");
@@ -36,7 +38,13 @@ class ReviewReportForm extends MenuForm
                     $player->sendForm($this);
                 }
             }elseif($data == 1){
-                $player->sendMessage(Report::getInstance()->prefix . "§csoon available!");
+                if($target->getServer()->getPluginManager()->getPlugin("cucumber")->getName() === "cucumber") {
+                    $newCommand = str_replace("{player}", $target->getName(), "ban {player} inf Banned by ReportSystem");
+                } else {
+                    $newCommand = str_replace("{player}", $target->getName(), "ban {player} Banned by ReportSystem");
+                }
+                $player->getServer()->dispatchCommand($player, $newCommand);
+                $player->sendMessage(Report::getInstance()->prefix . "§eBanned Sucessfully");
                 $player->sendForm($this);
             }elseif($data == 2){
                 Report::getInstance()->moveToRecycleBin($this->report);
